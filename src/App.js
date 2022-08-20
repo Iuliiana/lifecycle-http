05bb39e41063b50d03fdbcef1260c4ef9b059a1f
Iuliia <tretiakova.iuliia@gmail.com>
2022-08-20 16:45:53 +0300

    create app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {Routes, Route} from 'react-router-dom';
+import {Homepage} from "./pages/Homepage";
+import {Notfoundpage} from "./pages/Notfoundpage";
+import {Layout} from "./components/Layout";
+import {Watchespage} from "./pages/Watchespage";
+import {Crudpage} from "./pages/Crudpage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Homepage/>}/>
+          <Route path='watches' element={<Watchespage/>}/>
+          <Route path='crud' element={<Crudpage/>}/>
+          <Route path='*' element={<Notfoundpage/>}/>
+        </Route>
+      </Routes>
   );
 }
 
